7cf8c2436aeb1ad3e06d0d91b99c1c8a401261f6
Ndimofor <ndimaret@gmail.com>
2024-08-28 15:00:57 +0200

    add editable function

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,11 @@
-import { StoryblokComponent } from "@storyblok/react/rsc";
+import { StoryblokComponent, storyblokEditable } from "@storyblok/react/rsc";
 
 export const Page = (params: any) => {
   return (
-    <section>
+    <main {...storyblokEditable(params.blok)}>
       {params.blok.blocks.map((blok: any) => (
         <StoryblokComponent blok={blok} key={blok._uid} />
       ))}
-    </section>
+    </main>
   );
 };
